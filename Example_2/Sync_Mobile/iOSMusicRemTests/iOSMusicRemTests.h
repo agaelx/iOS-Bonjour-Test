bb23c9d966d049fa5320bbe0de2fbf881bbc24ca
Uriel Garcia <agaelx@gmail.com>
2014-06-05 22:09:42 -0600

    - examples
    - example 2

---
+++ Example_2/Sync_Mobile/iOSMusicRemTests/iOSMusicRemTests.h
@@ -0,0 +1,17 @@
+//
+//  iOSMusicRemTests.h
+//  iOSMusicRemTests
+//
+//  Created by Uriel on 03/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface iOSMusicRemTests : SenTestCase {
+@private
+    
+}
+
+@end
